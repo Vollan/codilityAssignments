b8aae3056b8c23c89d75a77f60c6f0d679afc338
Vollan <emil.sandstrom1986@gmail.com>
2017-04-09 13:10:57 +0200

    Update 3.2 permMissingElem.swift

--- 3.2 permMissingElem.swift
+++ 3.2 permMissingElem.swift
@@ -4,17 +4,17 @@ import UIKit
 
 public func solution(_ A : inout [Int]) -> Int {
     var s = Set<Int>()
-    var last: Int = 0
-    var correctNumber = 0
-    if A.isEmpty {correctNumber = last+1}
-    A.map({ s.insert($0) })
-    s.sorted().map({ if $0 == last+1 {
-        last = last+1
-    } else {
-            correctNumber = last+1
+    var next: Int = 1
+    
+    A.map({s.insert($0)})
+    
+    for i in s.sorted() {
+        if i != next {
+            return next
         }
-    })
-    return correctNumber == 0 ? last+1 : correctNumber
+        next+=1
+    }
+    return next
 }
 
 
